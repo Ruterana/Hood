ebc94bc905d765dda0d8a18867bd0cd4159f828e
Ruterana <ncharlene38@gmail.com>
2019-11-01 14:50:54 +0200

    iadd registration

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from django.contrib .auth.models import User
+from tinymce.models import HTMLField
 # Create your models here.
+class Profile(models.Model):
+    profile_image = models.ImageField(upload_to = 'pictures/')
+    bio= models.CharField(max_length=30)
+    user= models.OneToOneField(User,on_delete=models.CASCADE)
+    username= models.CharField(max_length=30)
+    location = models.CharField(max_length=33)
+    neighbourhood = models.CharField(max_length=32,null= True)
+def save_profile(self):
+        self.save()
+def delete_neighbourhood(self):
+        self.delete()  
+class post(models.Model):
+    name= models.CharField(max_length=30)  
+    user= models.ForeignKey(User)
+    post = HTMLField()
+    image_path = models.ImageField(upload_to = 'pictures/')
+def __str__(self):
+        return self.name
+
+def save_post(self):
+        self.save() 
+
+def delete_neighbourhood(self):
+        self.delete()  
+    
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=30)
+    location= models.CharField(max_length=32)
+    count  = models.IntegerField(default=0)
+def __str__(self):
+        return self.name
+
+def save_neighbourhood(self):
+        self.save() 
+
+def delete_neighbourhood(self):
+        self.delete()  
--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.http  import HttpResponse
 
@@ -8,3 +8,4 @@ def home(request):
     return HttpResponse('Welcome to  the neiborhood app')
 def home(request):
     return render(request, 'home.html')
+# @login_required(login_url='/accounts/login/')
